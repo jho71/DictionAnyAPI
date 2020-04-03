99ff9f504532cf50508300a9baad9dbaf48a8f8f
John Ho <jho71@myseneca.ca>
2020-04-03 14:21:49 -0400

    update schema

--- msc-englishTerm.js
+++ msc-englishTerm.js
@@ -6,31 +6,33 @@ var Schema = mongoose.Schema;
 // Entity schema
 
 var englishTermSchema = new Schema({
-  wordEnglish: {type: String, required: true, unique: true},
-  wordNonEnglish: String,
-  wordExpanded: String,
-  languageCode: String,
-  image: String,
-  imageType: String,
-  audio: String,
-  audioType: String,
-  linkAuthoritative: String,
-  linkWikipedia: String,
-  linkYouTube: String,
-  authorName: String,
-  dateCreated: {type: Date, default: Date.now},
-  dateRevised:  {type: Date, default: Date.now},
-  fieldOfStudy: String,
-  helpYes: Number,
-  helpNo: Number,
-    definitions: [{
-    authorName: String,
-    dateCreated: {type: Date, default: Date.now},
-    definition: String,
-    quality: Number,
-    likes: Number
-    }]
-  //  ,termEnglishId: String
+ 
+  wordEnglish: { type: String, required: true, unique: true },
+  wordNonEnglish: { type: String, default: '' },
+  wordExpanded: { type: String, default: '' },
+  languageCode: { type: String, required: true },
+  image: { type: String, default: '' },
+  imageType: { type: String, default: '' },
+  audio: { type: String, default: '' },
+  audioType: { type: String, default: '' },
+  linkAuthoritative: { type: String, default: '' },
+  linkWikipedia: { type: String, default: '' },
+  linkYouTube: { type: String, default: '' },
+  authorName: { type: String, required: true },
+  dateCreated: { type: Date, default: Date.now, required: true },
+  dateRevised: { type: Date, default: Date.now, required: true },
+  fieldOfStudy: { type: String, default: '' },
+  helpYes: { type: Number, default: 0 },
+  helpNo: { type: Number, default: 0 },
+
+  definitions: [{
+    authorName: { type: String, required: true },
+    dateCreated: { type: Date, default: Date.now },
+    definition: { type: String, required: true },
+    quality: { type: Number, default: 0 },
+    likes: { type: Number, default: 0 },
+  }]
+  
 });
 
 // Make schema available to the application
--- msc-nonEnglishTerm.js
+++ msc-nonEnglishTerm.js
@@ -6,33 +6,33 @@ var Schema = mongoose.Schema;
 // Entity schema
 
 var nonEnglishTermSchema = new Schema({
-  wordEnglish:  {type: String, required: true},
-  wordNonEnglish:{type: String, required: true, unique: true},
-  wordExpanded: String,
-  languageCode: {type: String, required: true},
-  image: String,
-  imageType: String,
-  audio: String,
-  audioType: String,
-  linkAuthoritative: String,
-  linkWikipedia: String,
-  linkYouTube: String,
-  authorName: String,
-  dateCreated: {type: Date, default: Date.now},
-  dateRevised:  {type: Date, default: Date.now},
-  fieldOfStudy: String,
-  helpYes: Number,
-  helpNo: Number,
-  termEnglishId: {  type: Schema.Types.ObjectId, ref: 'terms-english'},
-    definitions: [{
-    authorName: String,
-    dateCreated: {type: Date, default: Date.now},
-    definition: String,
-    quality: Number,
-    likes: Number
-    }]
-   
-});
+  wordEnglish: { type: String, required: true},
+  wordNonEnglish: { type: String, required : true, unique: true  },
+  wordExpanded: { type: String, default: '' },
+  languageCode: { type: String, required: true },
+  image: { type: String, default: '' },
+  imageType: { type: String, default: '' },
+  audio: { type: String, default: '' },
+  audioType: { type: String, default: '' },
+  linkAuthoritative: { type: String, default: '' },
+  linkWikipedia: { type: String, default: '' },
+  linkYouTube: { type: String, default: '' },
+  authorName: { type: String, required: true },
+  dateCreated: { type: Date, default: Date.now, required: true },
+  dateRevised: { type: Date, default: Date.now, required: true },
+  fieldOfStudy: { type: String, default: '' },
+  helpYes: { type: Number, default: 0 },
+  helpNo: { type: Number, default: 0 },
 
+  definitions: [{
+    authorName: { type: String, required: true },
+    dateCreated: { type: Date, default: Date.now },
+    definition: { type: String, required: true },
+    quality: { type: Number, default: 0 },
+    likes: { type: Number, default: 0 },
+  }],
+  termEnglishId: {  type: Schema.Types.ObjectId, ref: 'terms-english'}
+
+});
 // Make schema available to the application
 module.exports = nonEnglishTermSchema;
